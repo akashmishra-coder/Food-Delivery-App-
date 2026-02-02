b989bfacc407c8fd917c8ce854c1e602fd379c17
AkashMIshra <akashmishra247500@gmail.com>
2026-02-02 14:29:06 +0530

    css fix

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -2,8 +2,10 @@ const Shimmer = () => {
     
     return(
       <>
-         <div className=" text-md py-4 bg-red-200 h-15 md:text-center font-bold font-serif text-red-600 mb-3">
+         <div className=" text-md flex items-center justify-center bg-red-200 h-15 md:text-center font-bold font-serif text-red-600 mb-3">
+          <h3>
           This website uses the real-time API. Please use the CORS crome extention for to load the data. And refresh the page.          
+          </h3>
         </div>
         <div className="flex flex-wrap justify-center mt-10 gap-10 mb-10">
         <div className=" w-50 h-60 bg-gray-300 rounded-xl"></div>
